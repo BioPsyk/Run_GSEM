fe6c5fb8867edeed92e8897b92c2e6eb7f4396f3
vaqm2 <appadurai.vivek@gmail.com>
2022-01-05 17:57:31 +0100

    creates new dir for chrom split gwas

--- bin/run_gsem_stage1.R
+++ bin/run_gsem_stage1.R
@@ -45,11 +45,12 @@ saveRDS(LDSCoutput, paste0(out_prefix, "_LDSC.rds"))
 # ----- STEP 3: SPLIT SUMMARY STATS FOR GWAS -----
 
 for (i in 1:nrow(sum_stats)) {
+    dir.create(i, showWarnings = TRUE)
     file = fread(sum_stats$FILE_PATH[i], data.table = FALSE, header = T)
     for (chromosome in c(1:22)) {
         file_chr = file %>% filter(CHR == chromosome)
         write.table(file_chr,
-                    paste0(sum_stats$TRAIT[i], "_CHR", chromosome, ".split.assoc"),
+                    paste0(i, "/", sum_stats$TRAIT[i], "_CHR", chromosome, ".split.assoc"),
                     row.names = F,
                     quote = F,
                     sep = " ")
