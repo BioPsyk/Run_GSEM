fdcc2d6a1ab89dfc168808c6d164833edb8a3c61
vaqm2 <appadurai.vivek@gmail.com>
2022-01-25 15:32:19 +0100

    Added code to make double Manhattan plots

--- bin/miami_plot.R
+++ bin/miami_plot.R
@@ -1,11 +1,11 @@
 #!/usr/bin/env Rscript
 
-#- CODE TO MAKE A MIAMI PLOT for GENOMIC SEM OUTPUT HIGHLIGHTING ASSOCIATION 
-#- TEST P-VALUE AND HETEROGENEITY TEST Q_VALUE -----------------------------
+# - CODE TO MAKE A MIAMI PLOT for GENOMIC SEM OUTPUT HIGHLIGHTING ASSOCIATION 
+# - TEST P-VALUE AND HETEROGENEITY TEST Q_VALUE ---------------------------- #
 
 require(dplyr, quietly = TRUE)
 require(data.table, quietly = TRUE)
-require(ggplot2, quietly = TRUE)
+require(qqman, quietly = TRUE)
 
 args       = commandArgs(trailingOnly = TRUE)
 assoc      = fread(args[1], header = TRUE)
@@ -13,28 +13,53 @@ assoc      = assoc %>%
     filter(fail == 0) %>% 
     select(CHR, BP, Pval_Estimate, Q_pval)
 out_prefix = args[2]
-assoc_P    = assoc %>% select(CHR, BP, Pval_Estimate) %>% 
-    mutate(Test = "Association") %>% 
-    mutate(Highlight = ifelse(Pval_Estimate <= 5e-8, 1, 0)) %>% 
+assoc_P    = assoc %>% 
+    select(CHR, BP, Pval_Estimate) %>%
     rename(P = Pval_Estimate)
-assoc_Q    = assoc %>% select(CHR, BP, Q_pval) %>% 
-    mutate(Test = "Heterogeneity") %>% 
-    mutate(Highlight = ifelse(Q_pval <= 5e-8, 1, 0)) %>%
+assoc_Q    = assoc %>% 
+    select(CHR, BP, Q_pval) %>%
     rename(P = Q_pval)
-assoc      = rbind(assoc_P, assoc_Q)
+
+# --------------------------- MIAMI PLOT ---------------------------#
 
 png(paste0(out_prefix, "_MiamiPlot.png"), 
     res = 300, width = 10, height = 8, units = "in")
 
-ggplot(assoc, aes(x = BP, y = -1 * log10(P), color = as.factor(Highlight))) + 
-    geom_point() + 
-    theme_bw() + 
-    facet_grid(Test ~ CHR, space = "free") + 
-    theme(axis.text.x = element_blank(), legend.position = "none") +
-    xlab("") +
-    ylab("-log10(P)") + 
-    geom_hline(yintercept = -1 * log10(5e-8), lty = 2, color = "blue") +
-    geom_hline(yintercept = -1 * log10(1e-6), lty = 2, color = "green") + 
-    scale_color_manual(values = c("black", "red"))
-    
+par(mfrow = c(2, 1))
+par(mar = c(0, 5, 3, 3))
+manhattan(assoc_P,
+          ylim(0, max(assoc_P$P)), 
+          cex = 2.2, 
+          cex.lab = 2.5, 
+          font.lab = 2, 
+          font.axis = 2, 
+          cex.axis = 1.6, 
+          las = 2, 
+          font = 4, 
+          annotatePval = 5e-8)
+par(mar = c(5, 5, 3, 3))
+manhattan(assoc_Q, 
+          ylim = c(max(assoc_Q$P), 0),  
+          cex = 2.2, 
+          cex.lab = 2.5, 
+          font.lab = 2, 
+          font.axis = 2, 
+          cex.axis = 1.6,
+          las = 2, 
+          font = 4, 
+          xlab = "", 
+          xaxt = "n", 
+          annotatePval = 5e-8)
+dev.off()
+
+# --------------------------- QQ PLOT ---------------------------
+
+png(paste0(out_prefix, "_QQ.png"), 
+    res = 300, width = 8, height = 8, units = "in")
+par(mfrow = c(2, 1))
+par(mar = c(0, 5, 3, 3))
+qq(assoc_P$P)
+par(mar = c(5, 5, 3, 3))
+qq(assoc_Q$P)
+
 dev.off()
